403fac677c25ea9543711da855c82a19468d81cb
Casey Sugden <casey.sugden@gmail.com>
2022-03-01 10:23:51 -0700

    Solved challenge

--- makeUnique.js
+++ makeUnique.js
@@ -1,1 +1,32 @@
 // Write your solution below:
+
+// let makeUnique = word => {
+//     let uniqueWord = '';
+//         for(let i = 0; i < word.length; i++){
+//             if(!(uniqueWord.includes(word[i]))){
+//                 uniqueWord += word[i];
+//             };
+//         };
+//         return uniqueWord;
+// };
+
+// console.log(makeUnique('iwanttoclimbamountain'));
+
+let makeUnique2 = word => {
+    let uniqueWord = '';
+
+    for(let i = 0; i < word.length; i++){
+        let isUnique = true;
+        for(let j = 0; j < uniqueWord.length; j++){
+            if (word[i] === word[j]) {
+                isUnique = false;
+            }
+        }
+        if(isUnique){
+            uniqueWord += word[i];
+        }
+    }
+    console.log(uniqueWord);
+    return uniqueWord;
+}
+console.log(makeUnique2('hello'));
